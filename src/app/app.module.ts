3ad3a1149a32d37b33b0deb54e7b1217c8197560
Edward Cabrera <93951069+WardedC@users.noreply.github.com>
2024-01-15 19:39:48 -0400

    Gifs Interface was created and implemented on gifsService to catch the gifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     GifsModule,
     SharedModule
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,18 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-
+  public gifsList: Gif[] = []
   private _tagsHistory: string[] = []
+  private _apiKey: string = 'n0VnKOPEmyv3lbQiKikvklYNIGF50jU3'
+  private _serviceUrl: string = 'http://api.giphy.com/v1/gifs'
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 
   get tagHistory(){
@@ -22,9 +27,25 @@ export class GifsService {
     this._tagsHistory = this.tagHistory.splice(0,10);
   }
   
-  searchTag(tag:string): void{
+   searchTag(tag:string):void{
+
     if(tag.length === 0) return;
     this.organizeHistory(tag);
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('q', tag)
+      .set('limit', '10')
+      
+
+    this.http.get<SearchResponse>(`${this._serviceUrl}/search`, {params})
+      .subscribe(resp => {
+        this.gifsList = resp.data
+        console.log({gifs: this.gifsList})
+      })
+
+    
+      
+
   }
 
 }
