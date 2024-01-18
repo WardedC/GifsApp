b8b137fcbf2a6de0b297eea704c9ba04f599ab0a
Edward Cabrera <93951069+WardedC@users.noreply.github.com>
2024-01-18 10:33:46 -0400

    LazyImagenComponent was created and imported in gifsModule.

---
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazyImage.component.html',
+  styleUrl: './lazyImage.component.css',
+})
+export class LazyImageComponent {
+
+
+
+}
